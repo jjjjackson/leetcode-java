40f92eebdbef290aa02a0717062de42a1036a6a5
Jackson Chen <a@jackson.tw>
2021-06-10 01:03:38 +0900

    👍 add Interval and TreeNode structure

---
+++ src/main/java/leetcode/tw/jackson/structure/Interval.java
@@ -0,0 +1,11 @@
+package leetcode.tw.jackson.structure;
+
+public class Interval {
+
+    public int start, end;
+
+    public Interval(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+}
